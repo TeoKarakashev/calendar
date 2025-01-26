74cbd853be84b7925dd92ca52e8f67a2603e638c
Kristiyan Todorov <kristiyan.todorov@trading212.com>
2025-01-26 16:46:16 +0200

    used component

--- client/pages/login.php
+++ client/pages/login.php
@@ -19,17 +19,7 @@
   <script defer src="../scripts/navbar.js"></script>
 </head>
 <body>
-  <nav class="navbar">
-    <ul>
-    <li><a class="links" id="home-btn" href="./index.php">Home</a></li>
-      <li><a class="links" id="interests-btn" href="./interests.php">Interests</a></li>
-      <li><a class="links" id="my-profile-btn" href="./my-profile.php">My Profile</a></li>
-      <li><a class="links" id="presentation-btn" href="./presentations.php">Presentations</a></li>
-      <li><a class="links" id="sign-in-btn" href="./register.php">Register</a></li>
-      <li><a class="links" id="sign-up-btn" href="./login.php">Login</a></li>
-      <li><a class="links" id="logout-btn" onclick="logout(event)">Logout</a></li>
-    </ul>
-  </nav>
+  <?php include '../components/navbar.php'; ?>
   
   <div id="errors">
     <p class="error-msg"></p>
--- client/pages/register.php
+++ client/pages/register.php
@@ -17,17 +17,7 @@
   <link rel="stylesheet" href="../styles/register.css"/>
 </head>
 <body>
-  <nav class="navbar">
-    <ul>
-    <li><a class="links" id="home-btn" href="./index.php">Home</a></li>
-      <li><a class="links" id="interests-btn" href="./interests.php">Interests</a></li>
-      <li><a class="links" id="my-profile-btn" href="./my-profile.php">My Profile</a></li>
-      <li><a class="links" id="presentation-btn" href="./presentations.php">Presentations</a></li>
-      <li><a class="links" id="sign-in-btn" href="./register.php">Register</a></li>
-      <li><a class="links" id="sign-up-btn" href="./login.php">Login</a></li>
-      <li><a class="links" id="logout-btn" onclick="logout(event)">Logout</a></li>
-    </ul>
-  </nav>
+  <?php include '../components/navbar.php'; ?>
 
   <form id="register-form">
   <p class="form-title">Register</p>
